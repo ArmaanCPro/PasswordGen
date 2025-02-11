d086acd53795f33703637f42af0cd0207165129d
Armaan Chahal <armaan.c2009@gmail.com>
2025-02-10 22:14:28 -0600

    cli is updated a little bit, but still rudimentary

--- cli/src/cli.cpp
+++ cli/src/cli.cpp
@@ -5,33 +5,59 @@ int main()
 {
     Generator::PasswordPolicy policy;
     Generator::PasswordGenerator pwdGen(policy);
-    std::cout << "Password length?\n";
-    std::cin >> policy.passwordLength;
+    bool isRunning = true;
 
-    char buf;
-    std::cout << "Use symbols? y/n\n";
-    std::cin >> buf;
-    policy.requireSymbols = buf == 'y';
-    std::cout << "Use numbers? y/n\n";
-    std::cin >> buf;
-    policy.requireNumbers = buf == 'y';
-    std::cout << "Use uppercase? y/n\n";
-    std::cin >> buf;
-    policy.requireUppercase = buf == 'y';
-    std::cout << "Use lowercase? y/n\n";
-    std::cin >> buf;
-    policy.requireLowercase = buf == 'y';
-
-    std::cout << "Excluded characters? y/n\n";
-    std::cin >> buf;
-    if ( buf == 'y')
+    while (isRunning)
     {
-        std::cout << "Enter excluded characters: \n";
-        std::cin >> policy.excludedCharacters;
-    }
+        std::cout << "1. Set length\n2. Set use lowercase\n3. Set use uppercase\n4. Set use numbers"
+                     "\n5. Set use symbols\n6. Generate password\n7. Hash password\n8. Exit\n\nEnter your choice: " << std::flush;
 
-    pwdGen.SetPolicy(policy);
-    std::cout << "Password: " << std::endl;
-    std::cout << pwdGen.GenerateAdvancedPassword();
+        int choice = 0;
+        std::cin >> choice;
+
+        std::string password;
+
+        switch (choice)
+        {
+            case 1:
+                std::cout << "Enter length: " << std::endl;
+                std::cin >> policy.passwordLength;
+                break;
+            case 2:
+                policy.requireLowercase = !policy.requireLowercase;
+                std::cout << "Lowercase: " << policy.requireLowercase << std::endl;
+                break;
+            case 3:
+                policy.requireUppercase = !policy.requireUppercase;
+                std::cout << "Uppercase: " << policy.requireUppercase << std::endl;
+                break;
+            case 4:
+                policy.requireNumbers = !policy.requireNumbers;
+                std::cout << "Numbers: " << policy.requireNumbers << std::endl;
+                break;
+            case 5:
+                policy.requireSymbols = !policy.requireSymbols;
+                std::cout << "Symbols: " << policy.requireSymbols << std::endl;
+                break;
+            case 6:
+                password = pwdGen.GenerateAdvancedPassword();
+                std::cout << password << std::endl;
+                break;
+            case 7:
+                if (password.empty())
+                {
+                    std::cout << "No password is set, enter a password: " << std::endl;
+                    std::cin >> password;
+                }
+                std::cout << pwdGen.HashPasswordSafe(std::move(password)) << std::endl;
+                break;
+            case 8:
+                isRunning = false;
+                break;
+
+            default:
+                std::cout << "Invalid choice" << std::endl;
+        }
+    }
     return 0;
 }
